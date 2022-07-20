ed7015dff03405c77771440de3710e1659779c46
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2022-07-20 21:17:43 +0200

    Add AesSiv

---
+++ UnitTests/AesSiv_KAT.cs
@@ -0,0 +1,41 @@
+﻿// SPDX-FileCopyrightText: 2022 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+namespace UnitTests;
+
+[TestClass]
+sealed class AesSiv_KAT
+{
+    [TestMethod]
+    [TestCategory("RFC")]
+    [RfcAesSivTestVectorSource]
+    public void Rfc_Encrypt(RfcAesSivTestVector testVector)
+    {
+        using var aesSiv = new AesSiv(testVector.Key.ToArray());
+        var associatedData = new List<byte[]>(testVector.AD.Select(ad => ad.ToArray()));
+        if (testVector.Nonce.HasValue)
+        {
+            associatedData.Add(testVector.Nonce.Value.ToArray());
+        }
+        var ciphertext = new byte[AesSiv.BlockSize / 8 + testVector.Plaintext.Length];
+        aesSiv.Encrypt(testVector.Plaintext.ToArray(), ciphertext, associatedData.ToArray());
+        Assert.IsTrue(Enumerable.SequenceEqual(testVector.output.ToArray(), ciphertext));
+    }
+
+    [TestMethod]
+    [TestCategory("RFC")]
+    [RfcAesSivTestVectorSource]
+    public void Rfc_Decrypt(RfcAesSivTestVector testVector)
+    {
+        using var aesSiv = new AesSiv(testVector.Key.ToArray());
+        var associatedData = new List<byte[]>(testVector.AD.Select(ad => ad.ToArray()));
+        if (testVector.Nonce.HasValue)
+        {
+            associatedData.Add(testVector.Nonce.Value.ToArray());
+        }
+        var plaintext = new byte[testVector.output.Length - AesSiv.BlockSize / 8];
+        aesSiv.Decrypt(testVector.output.ToArray(), plaintext, associatedData.ToArray());
+        Assert.IsTrue(Enumerable.SequenceEqual(testVector.Plaintext.ToArray(), plaintext));
+    }
+}
---
+++ UnitTests/RfcAesSivTestVectorSourceAttribute.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-FileCopyrightText: 2022 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using System.Reflection;
+
+namespace UnitTests;
+
+[AttributeUsage(AttributeTargets.Method)]
+internal sealed class RfcAesSivTestVectorSourceAttribute
+    : Attribute
+    , ITestDataSource
+{
+    public RfcAesSivTestVectorSourceAttribute()
+    {
+    }
+
+    public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+    {
+        return RfcAesSivTestVector.All.Select(tv => new object[] { tv });
+    }
+
+    public string GetDisplayName(MethodInfo methodInfo, object[] data)
+    {
+        var testVector = (RfcAesSivTestVector)data[0];
+        return $"{methodInfo.Name}({testVector.Name})";
+    }
+}
