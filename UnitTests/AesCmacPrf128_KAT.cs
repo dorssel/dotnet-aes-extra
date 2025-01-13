93aa1c50c039cefe6c2bc060a0a68218e684841f
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2025-01-13 02:53:50 +0100

    Add AesCmacPrf128

---
+++ UnitTests/AesCmacPrf128_KAT.cs
@@ -0,0 +1,29 @@
+﻿// SPDX-FileCopyrightText: 2025 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+namespace UnitTests;
+
+[TestClass]
+sealed class AesCmacPrf128_KAT
+{
+    [TestMethod]
+    [TestCategory("RFC")]
+    [RfcAesCmacPrf128TestVectorSource]
+    public void Rfc_DeriveKey_Array_Array(RfcAesCmacPrf128TestVector testVector)
+    {
+        var output = AesCmacPrf128.DeriveKey(testVector.Key.ToArray(), testVector.Message.ToArray());
+        CollectionAssert.AreEqual(testVector.Output.ToArray(), output);
+    }
+
+    [TestMethod]
+    [TestCategory("RFC")]
+    [RfcAesCmacPrf128TestVectorSource]
+    public void Rfc_DeriveKey_ReadOnlySpan_ReadOnlySpan_Span(RfcAesCmacPrf128TestVector testVector)
+    {
+        var output = new byte[testVector.Output.Length];
+        int bytesWritten = AesCmacPrf128.DeriveKey(testVector.Key.Span, testVector.Message.Span, output);
+        Assert.AreEqual(bytesWritten, testVector.Output.Length);
+        CollectionAssert.AreEqual(testVector.Output.ToArray(), output);
+    }
+}
