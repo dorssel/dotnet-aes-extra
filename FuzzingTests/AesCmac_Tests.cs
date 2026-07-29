bbefa5f4ba4c9d3cda2499df8f2d2dad542c5d31
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2026-07-29 11:27:21 +0200

    Add initial fuzzing boilerplate

---
+++ FuzzingTests/AesCmac_Tests.cs
@@ -0,0 +1,20 @@
+﻿// SPDX-FileCopyrightText: 2026 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using FsCheck;
+using FsCheck.Fluent;
+
+namespace FuzzingTests;
+
+[TestClass]
+sealed class AesCmac_Tests
+{
+    [TestMethod]
+    public void FuzzingSomething()
+    {
+        using var aesCmac = new AesCmac();
+
+        Prop.ForAll<byte[]>(bytes => aesCmac.ComputeHash(bytes).Length == 16).QuickCheckThrowOnFailure();
+    }
+}
